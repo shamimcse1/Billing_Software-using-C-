e397d1b9a830afc02319d0a4903c34317719ade4
shamimcse1 <mdshamimh529@gmail.com>
2022-03-26 20:49:59 +0600

    first commit

---
+++ Billing_Software/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Billing_Software
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtToatalPrice = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.AddNew = new System.Windows.Forms.Button();
+            this.btnPriview = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.PrintDoc = new System.Drawing.Printing.PrintDocument();
+            this.PtintPriview = new System.Windows.Forms.PrintPreviewDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.PriductText = new System.Windows.Forms.TextBox();
+            this.QuantitText = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(805, 52);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(297, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Simple Billing Software";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name :";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(155, 98);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(224, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrice.Location = new System.Drawing.Point(155, 259);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 3;
+            this.txtPrice.TextChanged += new System.EventHandler(this.txtPrice_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Product Name :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Quantity :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 265);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 16);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Price :";
+            // 
+            // txtToatalPrice
+            // 
+            this.txtToatalPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtToatalPrice.Location = new System.Drawing.Point(155, 318);
+            this.txtToatalPrice.Name = "txtToatalPrice";
+            this.txtToatalPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtToatalPrice.TabIndex = 0;
+            this.txtToatalPrice.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(37, 324);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 16);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Total Price :";
+            // 
+            // AddNew
+            // 
+            this.AddNew.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.AddNew.Image = global::Billing_Software.Properties.Resources.download;
+            this.AddNew.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.AddNew.Location = new System.Drawing.Point(425, 98);
+            this.AddNew.Name = "AddNew";
+            this.AddNew.Size = new System.Drawing.Size(115, 24);
+            this.AddNew.TabIndex = 5;
+            this.AddNew.Text = "Add New ";
+            this.AddNew.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.AddNew.UseVisualStyleBackColor = true;
+            this.AddNew.Click += new System.EventHandler(this.AddNew_Click);
+            // 
+            // btnPriview
+            // 
+            this.btnPriview.Location = new System.Drawing.Point(425, 133);
+            this.btnPriview.Name = "btnPriview";
+            this.btnPriview.Size = new System.Drawing.Size(115, 23);
+            this.btnPriview.TabIndex = 6;
+            this.btnPriview.Text = "Print Priview";
+            this.btnPriview.UseVisualStyleBackColor = true;
+            this.btnPriview.Click += new System.EventHandler(this.btnPriview_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(425, 171);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(115, 23);
+            this.btnPrint.TabIndex = 7;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            // 
+            // PrintDoc
+            // 
+            this.PrintDoc.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.PrintDoc_PrintPage);
+            // 
+            // PtintPriview
+            // 
+            this.PtintPriview.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.PtintPriview.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.PtintPriview.ClientSize = new System.Drawing.Size(400, 300);
+            this.PtintPriview.Enabled = true;
+            this.PtintPriview.Icon = ((System.Drawing.Icon)(resources.GetObject("PtintPriview.Icon")));
+            this.PtintPriview.Name = "PtintPriview";
+            this.PtintPriview.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(425, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.OrangeRed;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 367);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(805, 51);
+            this.panel2.TabIndex = 8;
+            // 
+            // PriductText
+            // 
+            this.PriductText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PriductText.Location = new System.Drawing.Point(155, 156);
+            this.PriductText.Name = "PriductText";
+            this.PriductText.Size = new System.Drawing.Size(224, 20);
+            this.PriductText.TabIndex = 1;
+            // 
+            // QuantitText
+            // 
+            this.QuantitText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuantitText.Location = new System.Drawing.Point(155, 195);
+            this.QuantitText.Name = "QuantitText";
+            this.QuantitText.Size = new System.Drawing.Size(224, 20);
+            this.QuantitText.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(425, 210);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(115, 23);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 418);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnPriview);
+            this.Controls.Add(this.AddNew);
+            this.Controls.Add(this.txtToatalPrice);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.QuantitText);
+            this.Controls.Add(this.PriductText);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtToatalPrice;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button AddNew;
+        private System.Windows.Forms.Button btnPriview;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Drawing.Printing.PrintDocument PrintDoc;
+        private System.Windows.Forms.PrintPreviewDialog PtintPriview;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox PriductText;
+        private System.Windows.Forms.TextBox QuantitText;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Billing_Software/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Billing_Software
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AddNew_Click(object sender, EventArgs e)
+        {
+            txtName.Clear();
+            PriductText.Clear();
+            QuantitText.Clear();
+            txtPrice.Clear();
+            txtToatalPrice.Clear();
+       
+        }
+
+        private void txtPrice_TextChanged(object sender, EventArgs e)
+        {
+            UpdateIndex();
+
+        }
+
+        private void UpdateIndex()
+        {
+
+            if (QuantitText.Text != "" && txtPrice.Text != "")
+            {
+                double TotalPrice = Convert.ToInt64(QuantitText.Text) * Convert.ToInt64(txtPrice.Text);
+                txtToatalPrice.Text = TotalPrice.ToString() + ".00";
+            }
+            else
+            {
+                txtToatalPrice.Text = "0";
+            }
+
+
+        }
+
+        private void comboQuat_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateIndex();
+        }
+
+        private void btnPriview_Click(object sender, EventArgs e)
+        {
+            PtintPriview.Document = PrintDoc;
+            PtintPriview.Show();
+            this.Hide();
+        }
+
+        private void PrintDoc_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            e.Graphics.DrawString("...................Hello Programmer...................\n" + "\n\n Customar Name :"+txtName.Text+ "\n\n Product Name :"+ PriductText.Text+ "\n\n Quantity :" + QuantitText.Text+ "\n\n Price :" + txtPrice.Text
+                + "\n\n Total Price  :" + txtToatalPrice.Text, new Font("Arial", 20, FontStyle.Bold), Brushes.Black, new Point(140, 160));
+
+            saveData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void saveData()
+        {
+
+            try
+            {
+                //TextWriter txt = new StreamWriter("C:\\demo\\demo.txt");
+                TextWriter txt = new StreamWriter("All Data.txt");
+                if (txt != null)
+                {
+                    txt.Write("NAME:-"+txtName.Text + "\n");
+                    txt.Write("PRODUCT NAME:-" + PriductText.Text + "\n");
+                    txt.Write("QUANTITY:-" + QuantitText.Text + "\n");
+                    txt.Write("PRICE:-" + txtPrice.Text + "\n");
+                    txt.Write("TOTAL PRICE:-" + txtToatalPrice.Text + "\n");
+                    txt.Close();
+                   // MessageBox.Show("Save Successfully");
+                }
+                else
+                {
+                    MessageBox.Show("Please Fil The Box");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+
+            }
+
+            
+            
+
+
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            saveData();
+
+        }
+    }
+}
